878d688b99a0a264ada4a13de252f00ce906684b
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2021-12-19 01:32:02 +0800

    test

--- node_modules/@commitlint/load/lib/utils/load-parser-opts.d.ts
+++
@@ -1,3 +0,0 @@
-import { ParserPreset } from '@commitlint/types';
-export declare function loadParserOpts(pendingParser: string | ParserPreset | Promise<ParserPreset> | undefined): Promise<ParserPreset | undefined>;
-//# sourceMappingURL=load-parser-opts.d.ts.map
--- node_modules/@commitlint/load/lib/utils/pick-config.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const pickConfig: (input: unknown) => Record<string, unknown>;
-//# sourceMappingURL=pick-config.d.ts.map
